e937f3fbd91ffd2793c4946cd0d2bfd37d610a43
gregwhitaker <gwhitake@gmail.com>
2018-11-15 15:00:32 -0800

    Doing work

--- client/src/main/java/io/netifi/proteus/s3upload/client/ClientRunner.java
+++ client/src/main/java/io/netifi/proteus/s3upload/client/ClientRunner.java
@@ -15,14 +15,51 @@
  */
 package io.netifi.proteus.s3upload.client;
 
+import com.google.protobuf.ByteString;
+import io.netifi.proteus.s3upload.service.protobuf.FilePart;
+import io.netifi.proteus.s3upload.service.protobuf.UploadServiceClient;
+import io.netifi.proteus.spring.core.annotation.Group;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 
 @Component
 public class ClientRunner implements CommandLineRunner {
 
+    @Group("services.example.s3upload")
+    private UploadServiceClient client;
+
     @Override
     public void run(String... args) throws Exception {
+        File fileToUpload = null;
+        if (0 < args.length) {
+            fileToUpload = new File(args[0]);
+        } else {
+            throw new IllegalArgumentException("Missing fileToUpload argument");
+        }
 
+        final File f = fileToUpload;
+        client.upload(s -> {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(f);
+                DataInputStream inputStream = new DataInputStream(fileInputStream);
+
+                while (inputStream.available() > 0) {
+                    byte[] buf = new byte[128];
+                    inputStream.read(buf);
+
+                    s.onNext(FilePart.newBuilder()
+                            .setPart(ByteString.copyFrom(buf))
+                            .build());
+                }
+
+                s.onComplete();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
